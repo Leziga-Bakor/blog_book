eb617807b88b8d548f689d902ee982036e8c9016
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-06 02:09:04 +0100

    Created Post model

--- blogbook.py
+++ blogbook.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
@@ -15,11 +16,20 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file=db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts=db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
+class Post(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id=db.Column(db.Integer, db.foreignKey('user.id'), nullable=False)
 
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 posts = [
     {
