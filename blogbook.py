04c393c6afb1baf90243111a8b3bf8c2f67a16eb
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-08 22:26:37 +0100

    Created models.py and moved models from blogbook.py

--- blogbook.py
+++ blogbook.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timezone
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
@@ -12,27 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file=db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts=db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-    
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default = datetime.now(timezone.utc))
-    content = db.Column(db.Text, nullable=False)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}','{self.date_posted}')"
+from models import User, Post
 
 posts = [
     {
