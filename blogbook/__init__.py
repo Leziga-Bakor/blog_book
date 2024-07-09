7b5555dd190b4982a5ec61a63bcf44775b588e51
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-09 14:37:16 +0100

    Added login manager from flask_login

--- blogbook/__init__.py
+++ blogbook/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -13,6 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
-
+login_manager=LoginManager(app)
 
 from blogbook import routes
