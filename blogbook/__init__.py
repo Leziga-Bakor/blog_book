18834126d2cb3e62b424f83b6093fcbb361aefe0
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-10 23:14:50 +0100

    Added category for message alert for login view

--- blogbook/__init__.py
+++ blogbook/__init__.py
@@ -14,5 +14,7 @@ migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from blogbook import routes
