e782f3d2e6f96b65ba1b8723fdf98c6cd5e54dcf
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-12 22:19:47 +0100

    Created form for post

--- blogbook/forms.py
+++ blogbook/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blogbook.models import User
 
@@ -47,3 +47,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email already exists. Please try another one.')
+            
+class PostForm(FlaskForm):
+    title=StringField('Title', validators=[DataRequired()])
+    content=TextAreaField('Content',validators=[DataRequired()])
+    submit = SubmitField('Post')
