0a57005c44cc2c5cdc270268b53b6dceb1c7a759
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-19 02:27:45 +0100

    feat: set up configuration for email

--- blogbook/__init__.py
+++ blogbook/__init__.py
@@ -22,6 +22,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
 
