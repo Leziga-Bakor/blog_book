cc5c63510ac2d5897ab0880029da3dd777083b24
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-21 23:26:21 +0100

    feat: register erros blueprint into app

--- blogbook/__init__.py
+++ blogbook/__init__.py
@@ -30,8 +30,10 @@ def create_app(config_class=Config):
     from blogbook.users.routes import users
     from blogbook.posts.routes import posts
     from blogbook.main.routes import main
+    from blogbook.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
